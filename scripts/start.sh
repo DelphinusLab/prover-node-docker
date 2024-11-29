873701040e99a12e3a9d90a19f6dfb15ff1fed40
rhaoio <richard@rhao.io>
2024-11-29 11:28:51 +1100

    ftp container in same file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,2 @@
-# Start FTP server containing param files first
-# This can remain running between prover node upgrades
-docker compose -f ftp-docker-compose.yml up -d
-
 # Start prover node services
 docker compose up
