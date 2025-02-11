a6246b0d580b68e8516f74a0dc23252a7025c5f8
rhaoio <richard@rhao.io>
2025-02-11 16:55:21 +1100

    attach fix

--- scripts/start_attach.sh
+++ scripts/start_attach.sh
@@ -1,3 +1,3 @@
 docker compose down  # Stop any existing services
 
-docker compose up --attach mongodb params-ftp prover-dry-run-service prover-node
+docker compose up --attach mongodb --attach params-ftp --attach prover-dry-run-service --attach prover-node
