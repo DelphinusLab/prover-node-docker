bf826feaf88ee3a569c0b4008bb3b94103414b3c
rhaoio <richard@rhao.io>
2025-03-19 19:52:08 +0900

    try stop mongodb in stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,8 @@
+try_stop_zkwasm_mongodb() {
+    # also try stop previous zkwasm-mongodb service
+    docker container stop zkwasm-mongodb &> /dev/null
+    docker container rm zkwasm-mongodb &> /dev/null
+}
+
 # Stop prover node services
 docker compose down
